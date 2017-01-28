3713ed26b9c7c4a19c158536d6360f55c4657678
byackee <vincent.fresnel@gmail.com>
2017-01-28 22:20:58 +0100

    Update spljeedom.sh

--- spljeedom.sh
+++ spljeedom.sh
@@ -2,9 +2,14 @@
 folder=$1
 duration=$2
 hw=$3
+url=votre url ici
+apikey=votre clef api jeedom
+devideid=votre devide id 
+paramid= param2842
+
 while true; do 
     arecord -q -D $hw -f cd -d $duration -t raw $folder/noise.wav
     amplitude=$(lame -r $folder/noise.wav 2>&1 | sed -n '/+/,/db/p' | sed 's/\ReplayGain: +//' | sed 's/\dB//' | sed -n 1p)
     amplitude=$(echo "scale=2; $amplitude * -1" | bc -l)
-    curl -s -d "apikey=renseigner ici votre clef api jeedom&type=httpRemoteEvent&eqId=189&param2842=$amplitude" http://votre ip jeedom/core/api/jeeApi.php >/dev/null 2>&1
+    curl -s -d "apikey=$apikey&type=httpRemoteEvent&eqId=$deviceid&$paramid=$amplitude" $url/core/api/jeeApi.php >/dev/null 2>&1
 done
